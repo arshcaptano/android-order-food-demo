7fd08e43c23775daa4a36b528f9e80c2bbb81b41
Captano <arshfodcc@gmail.com>
2019-04-03 22:20:14 +0300

    Files upload

---
+++ app/src/main/java/io/captano/fotelpoc/view/activity/DemoActivity.java
@@ -0,0 +1,25 @@
+package io.captano.fotelpoc.view.activity;
+
+import android.os.Build;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Window;
+import android.view.WindowManager;
+
+import io.captano.fotelpoc.R;
+
+public class DemoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_demo);
+
+        if (Build.VERSION.SDK_INT >= 21) {
+            Window w = getWindow();
+            w.addFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
+            w.addFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_NAVIGATION);
+            w.setFlags(WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS, WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS);
+        }
+    }
+}
